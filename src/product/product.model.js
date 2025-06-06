faff7f81798eb1aec63b6aaede1823db1a90eec0
Informatica <informatica@alumno.local>
2025-06-06 11:55:47 -0600

    feat: se agrego el modelo de product

---
+++ src/product/product.model.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        maxLength: [35, "Name cannot exceed 35 characters"]
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ["Product", "Service"]
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+        maxLength: [500, "Description cannot exceed 500 characters"]
+    }
+});
+
+userSchema.methods.toJSON = function () {
+    const { password, _id, ...product } = this.toObject();
+    product.uid = _id;
+    return product;
+}
+
+export default model("Product", productSchema);
