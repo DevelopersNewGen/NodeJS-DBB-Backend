a911dd35711baba9ac0795a54ee623484925dbb3
AngelJTumG <javierangel43.lo@gmail.com>
2025-06-25 21:16:04 -0600

    fix: versiones arregladas

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,23 +1,27 @@
 import {Router} from 'express';
-import { createUser, listUsers, getUserById, updateUserById, deleteUser, updateUser, updateUserPassword, favoriteAccount} from './user.controller.js';
-import { createUserValidator, listUsersValidator, getUserByIdValidator, updateUserAdminValidator, favoriteAccountValidator ,
-    deleteUserValidator, updateUserValidator, updatePasswordValidator} from '../middlewares/user-validator.js';
+import { createUser, getUsers, getUserById, updateUser, updateUserPassword, deleteUserAdmin, updateUserAdmin, updateRole, getUserLogged, favoriteAccount} from './user.controller.js';
+import { createUserValidator, listUsersValidator, getUserByIdValidator, 
+    updateUserValidator, updatePasswordValidator, deleteUserValidatorAdmin, updateUserValidatorAdmin, validateUpdateRole, getRoleValidator,favoriteAccountValidator } from '../middlewares/user-validator.js';
 
 const router = Router();
 
 router.post('/createUser', createUserValidator, createUser);
 
-router.get('/listUsers', listUsersValidator, listUsers);
+router.get('/', listUsersValidator, getUsers);
 
-router.get('/getUserById/:uid', getUserByIdValidator, getUserById);
+router.get('/findUser/:uid', getUserByIdValidator, getUserById);
 
-router.put('/updateUserById/:uid', updateUserAdminValidator, updateUserById);
-
-router.delete('/deleteUser/:uid', deleteUserValidator, deleteUser);
+router.delete("/deleteUserAdmin/:uid", deleteUserValidatorAdmin, deleteUserAdmin);
 
 router.put('/updateUser', updateUserValidator, updateUser);
 
-router.patch('/updateUserPassword', updatePasswordValidator, updateUserPassword);
+router.put("/updateUserAdmin/:uid", updateUserValidatorAdmin, updateUserAdmin);
+
+router.patch('/updatePassword', updatePasswordValidator, updateUserPassword);
+
+router.patch("/updateRole/:uid", validateUpdateRole, updateRole);
+
+router.get("/getUser", getRoleValidator, getUserLogged)
 
 router.patch("/favoriteAccount", favoriteAccountValidator, favoriteAccount);
 
